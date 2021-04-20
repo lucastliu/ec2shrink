8f6ed69511b29471d743da61d0788ed282e212a1
Lucas Liu <lucas.liu@duke.edu>
2021-04-19 22:24:57 -0400

    changed setup url

--- setup.py
+++ setup.py
@@ -3,21 +3,20 @@ from setuptools import setup
 setup(
   name='ec2shrink',
   packages=['ec2shrink'],
-  version='1.0.2',
+  version='1.1.0',
   license='GPLv3',
   description='An easier way to reduce the storage size for EC2.',
-  long_description='AWS CLoudShell CLI tool intended to make shrinking EC2 EBS Storage easier',
+  long_description='CLI tool intended to make shrinking EC2 EBS Storage easier',
   long_description_content_type='text/plain',
   author='Lucas Liu',
   author_email='lucas.liu.0000@gmail.com',
   url='https://github.com/lucastliu/ec2shrink',
-  download_url='https://github.com/lucastliu/ec2shrink/archive/pypi-0_1_3.tar.gz',
   keywords=['amazon', 'aws', 'ec2', 'storage'],
   install_requires=[
-          'awscli',
           'ec2instanceconnectcli',
           'click',
           'pexpect',
+          # 'awscli'  # do not use this for AWS CloudShell release, only for other devices
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',
